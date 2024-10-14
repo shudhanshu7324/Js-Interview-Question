692d33dfb3a42953848d7989df7292ded29afff7
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-10-14 11:47:22 +0530

    slice and splice

---
+++ SliceAndSplice/Slice.js
@@ -0,0 +1,8 @@
+const arr = [1,2,3,4,5];
+// const slicedArray = arr.slice(1,4);
+// console.log(slicedArray); // 2,3,4
+
+let slicedArray = arr.slice(2);
+console.log(slicedArray); // 3,4,5
+console.log(arr);
+
---
+++ SliceAndSplice/Splice.js
@@ -0,0 +1,6 @@
+const arr = [1,2,3,4,5,6];
+const splicedArray = arr.splice(2,2);
+const splicedArray = arr.splice(2,2,55,77);
+console.log(splicedArray);
+console.log(arr);
+
